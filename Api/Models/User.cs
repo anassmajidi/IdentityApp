f0cda08002f9bf9bf64078e78ddd9361f899f8b9
anassmajidi <76888140+anassmajidi@users.noreply.github.com>
2024-06-22 23:15:38 +0100

    02. Api project setup, AspNetCore Identity added

---
+++ Api/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Api.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,16 @@
 
+using Api.Data;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Api
 {
@@ -18,6 +27,52 @@ namespace Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            builder.Services.AddDbContext<Context>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            //be able to inject JWTService class inside our Controllers
+            builder.Services.AddScoped<JwtService>();
+
+            builder.Services.AddIdentityCore<User>(options =>
+            {
+                // password configuration
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                // for email confirmation
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddRoles<IdentityRole>() // be able add roles
+                .AddRoleManager<RoleManager<IdentityRole>>() // be able to make use of RoleManager
+                .AddEntityFrameworkStores<Context>() // providing our context
+                .AddSignInManager<SignInManager<User>>() // make user of Signin manager
+                .AddUserManager<UserManager<User>>() // make use of UserMnager to create users
+                .AddDefaultTokenProviders(); // be able to create tokens for email confirmation
+
+            // be able to authenticate users using JWT
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        // validate the token based on the key we have provided inside appsettings.development.json JWT:Key
+                        ValidateIssuerSigningKey = true,
+                        // the issuer singnin key based on JWT:Key
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+                        // the issuer which in here is the api project url we are using
+                        ValidIssuer = builder.Configuration["JWT:Issuer"],
+                        // validate the issuer (who ever is issuing the JWT)
+                        ValidateIssuer = true,
+                        // don't validate audience (angular side)
+                        ValidateAudience = false
+                    };
+                });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -29,6 +84,9 @@ namespace Api
 
             app.UseHttpsRedirection();
 
+            //adding UseAuthentication into our pipeline and this should come before UseAuthorization
+            // Authentication verifies the identity of a user or service, and authorization determines their access rights.
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
